ef0f34cde8bf18ec943427579b8520034b960e28
Erwin Steffens <erwinsteffens@gmail.com>
2017-02-21 22:06:26 +0100

    Convert indents to spaces

--- index.js
+++ index.js
@@ -1,96 +1,96 @@
 const Influx = require('influx'),
-	  config = require('config'),
-	  P1 = require('./lib/p1')
+      config = require('config'),
+      P1 = require('./lib/p1')
 
 const influxConfig = config.get('influxdb'),
-	  p1Config = config.get('p1')
+      p1Config = config.get('p1')
 
 var lastGasValue
 
 const influx = new Influx.InfluxDB({
-	host: influxConfig.host,
-	database: influxConfig.database,
-	username: influxConfig.username,
-	password: influxConfig.password,
-	schema: [
-		{
-			measurement: 'electricity',
-			tags: [],
-			fields: {
-				received: Influx.FieldType.FLOAT,
-				received_value_low: Influx.FieldType.FLOAT,
-				received_value_high: Influx.FieldType.FLOAT
-			}
-		},
-		{
-			measurement: 'gas',
-			tags: [],
-			fields: {
-				received: Influx.FieldType.FLOAT,
-				received_value: Influx.FieldType.FLOAT,
-			}
-		}
-	]
+    host: influxConfig.host,
+    database: influxConfig.database,
+    username: influxConfig.username,
+    password: influxConfig.password,
+    schema: [
+        {
+            measurement: 'electricity',
+            tags: [],
+            fields: {
+                received: Influx.FieldType.FLOAT,
+                received_value_low: Influx.FieldType.FLOAT,
+                received_value_high: Influx.FieldType.FLOAT
+            }
+        },
+        {
+            measurement: 'gas',
+            tags: [],
+            fields: {
+                received: Influx.FieldType.FLOAT,
+                received_value: Influx.FieldType.FLOAT,
+            }
+        }
+    ]
 })
 
 const sp = new P1.Reader({
-	port: p1Config.port,
-	portConfig: {
-		baudRate: p1Config.baudRate
-	}
+    port: p1Config.port,
+    portConfig: {
+        baudRate: p1Config.baudRate
+    }
 })
 
 sp.on('reading', function (packet) {
-	var points = [];
+    var points = [];
 
-	var gasValue = packet.gas.reading;
-	if (lastGasValue)
-	{
-		var gasUsed = gasValue - lastGasValue
+    var gasValue = packet.gas.reading;
+    if (lastGasValue)
+    {
+        var gasUsed = gasValue - lastGasValue
 
-		if (gasUsed) {
-			if (p1Config.debug) {
-				console.log(`Gas. Value: ${gasValue}, Last value: ${lastGasValue}, Used: ${gasUsed}`)
-			}
+        if (gasUsed) {
+            if (p1Config.debug) {
+                console.log(`Gas. Value: ${gasValue}, Last value: ${lastGasValue}, Used: ${gasUsed}`)
+            }
 
-			points.push({
-				measurement: 'gas',
-				fields: { 
-					received: gasUsed,
-					received_value: gasValue
-				},
-			});
-		}
-	}
-	lastGasValue = gasValue;
+            points.push({
+                measurement: 'gas',
+                fields: { 
+                    received: gasUsed,
+                    received_value: gasValue
+                },
+            });
+        }
+    }
+    lastGasValue = gasValue;
 
-	var electricityUsed = packet.electricity.received.actual.reading;
-	if (electricityUsed) {
-		var electricityReceivedLow = packet.electricity.received.tariff1.reading;
-		var electricityReceivedHigh = packet.electricity.received.tariff2.reading;
+    var electricityUsed = packet.electricity.received.actual.reading;
+    if (electricityUsed) {
+        var electricityReceivedLow = packet.electricity.received.tariff1.reading;
+        var electricityReceivedHigh = packet.electricity.received.tariff2.reading;
 
-		if (p1Config.debug) 
-		{
-			console.log(`Electricity. Low: ${electricityReceivedLow}, High: ${electricityReceivedHigh}, Used: ${electricityUsed}`)	
-		}
+        if (p1Config.debug) 
+        {
+            console.log(`Electricity. Low: ${electricityReceivedLow}, High: ${electricityReceivedHigh}, Used: ${electricityUsed}`)	
+        }
 
-		points.push({
-			measurement: 'electricity',
-			fields: { 
-				received: electricityUsed,
-				received_value_low: electricityReceivedLow,
-				received_value_high: electricityReceivedHigh
-			},
-		});
-	}
+        points.push({
+            measurement: 'electricity',
+            fields: { 
+                received: electricityUsed,
+                received_value_low: electricityReceivedLow,
+                received_value_high: electricityReceivedHigh
+            },
+        });
+    }
 
-	if (points.length) {
-		influx.writePoints(points).catch(err => {
-			console.error('Error writing to InfluxDB',  err)
-		})
-	}
+    if (points.length) {
+        influx.writePoints(points).catch(err => {
+            console.error('Error writing to InfluxDB',  err)
+        })
+    }
 })
 
 sp.on('error', function (error) {
-	console.error('P1Reader error', error)
+    console.error('P1Reader error', error)
 })
